3def3d5854fa9b0bf42708e98d9f3e84623f0aa4
stefan-zobel <stefan-zobel@users.noreply.github.com>
2021-07-20 22:48:32 +0200

    add distance() methods

--- src/test/java/net/cramer/simd/TestInitializeSIMD.java
+++ src/test/java/net/cramer/simd/TestInitializeSIMD.java
@@ -7,5 +7,7 @@ public final class TestInitializeSIMD {
         System.out.println(SIMD.l2normFloat(null, 0));
         System.out.println(SIMD.approxEqualDouble(null, null, 0, 0.01, 0.01));
         System.out.println(SIMD.approxEqualFloat(null, null, 0, 0.01f, 0.01f));
+        System.out.println(SIMD.distanceDouble(null, null, 0));
+        System.out.println(SIMD.distanceFloat(null, null, 0));
     }
 }
